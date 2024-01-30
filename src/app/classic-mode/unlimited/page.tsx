cd8a2c691fc817c69a011f9582da63f21a04f550
NGustavo011 <gsn181200@gmail.com>
2024-01-30 16:59:37 -0300

    refactor: remove unused test entry cards

--- src/app/classic-mode/unlimited/page.tsx
+++ src/app/classic-mode/unlimited/page.tsx
@@ -13,59 +13,7 @@ const getCards = (async () => {
 
 export default async function ClassicUnlimitedMode() {
   const {cards} = await getCards()
-  const t = [
-    {
-      id: "any_id_1",
-      name: "any_name1",
-      race: "any_race",
-      type: "any_type",
-      archetype: "any_archetype",
-      attribute: "any_attribute",
-      description: "any_description",
-      frameType: "any_frame_type",
-      imageUrl: "any_image_url",
-      imageUrlSmall: "any_image_url_small",
-      imageUrlCropped: "any_image_url_cropped",
-      atk: 10,
-      def: 10,
-      level: 10,
-      available: true,
-    },
-    {
-      id: "any_id_2",
-      name: "any_name2",
-      race: "any_race",
-      type: "any_type",
-      archetype: "any_archetype",
-      attribute: "any_attribute",
-      description: "any_description",
-      frameType: "any_frame_type",
-      imageUrl: "any_image_url",
-      imageUrlSmall: "any_image_url_small",
-      imageUrlCropped: "any_image_url_cropped",
-      atk: 10,
-      def: 10,
-      level: 10,
-      available: true,
-    },
-    {
-      id: "any_id_3",
-      name: "any_name3",
-      race: "any_race",
-      type: "any_type",
-      archetype: "any_archetype",
-      attribute: "any_attribute",
-      description: "any_description",
-      frameType: "any_frame_type",
-      imageUrl: "any_image_url",
-      imageUrlSmall: "any_image_url_small",
-      imageUrlCropped: "any_image_url_cropped",
-      atk: 10,
-      def: 10,
-      level: 10,
-      available: true,
-    },
-  ]
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
